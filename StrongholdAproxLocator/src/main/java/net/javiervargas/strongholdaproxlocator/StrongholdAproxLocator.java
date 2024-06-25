0a42ebc79901d30ceb7d5019ea20876a0a9e2a98
Gk <145079256+CaptainGuuk@users.noreply.github.com>
2024-06-25 04:56:57 -0600

    Add files via upload

---
+++ StrongholdAproxLocator/src/main/java/net/javiervargas/strongholdaproxlocator/StrongholdAproxLocator.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package net.javiervargas.strongholdaproxlocator;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author melissavargas
+ */
+public class StrongholdAproxLocator {
+
+    public static void main(String[] args) {
+
+                System.out.println("URSARING!!!!");
+                JFrame Frame = new JFrame();
+                Frame.setSize(300,275);
+                Panel MainWindow = new Panel();
+                Frame.add(MainWindow);
+                Frame.setVisible(true);
+    }
+}
